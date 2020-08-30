3bfdf91d66a34f018cd103c39095b317248c2bf0
Reetik Chandra <61019089+reetik8@users.noreply.github.com>
2020-08-30 08:14:32 -0400

    Add files via upload

---
+++ DataPiratesTeam/Models/InfoS.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace DataPiratesTeam.Models
+{
+    public class InfoS
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(100, MinimumLength = 3)]
+        [Required]
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Major { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Degree { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string University { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Interest { get; set; }
+        [Display(Name = "Work Experience")]
+        public string Work_Experience { get; set; }
+
+        [StringLength(5, MinimumLength = 1)]
+        [Required]
+        public string Gpa { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        [Display(Name = "Graduation Date")]
+        public string Graduation_Date { get; set; }
+    }
+}
---
+++ DataPiratesTeam/Models/SeedData.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+namespace DataPiratesTeam.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesInfoSContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesInfoSContext>>()))
+            {
+                if (context.InfoS.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.InfoS.AddRange(
+
+                    new InfoS
+                    {
+                        Name = "Akshita Lathar",
+                        Email = "akshitalathar@gmail.com",
+                        Phone = "0000000000",
+                        Address = "123 xyz",
+                        Major = "Computer Science",
+                        Degree = "Bachelors",
+                        University = "Miami University",
+                        Interest = "Artificial Intelligence",
+                        Work_Experience = "Web developement",
+                        Gpa = "4",
+                        Graduation_Date = "2021"
+                    },
+
+                    new InfoS
+                    {
+                        Name = "Bhuvan Shah",
+                        Email = "bhuvanshah@gmail.com",
+                        Phone = "0000000000",
+                        Address = "123 xyz",
+                        Major = "Software E",
+                        Degree = "Masters",
+                        University = "Miami University",
+                        Interest = "Analytics",
+                        Work_Experience = "Startup",
+                        Gpa = "4",
+                        Graduation_Date = "2023"
+                    },
+
+                    new InfoS
+                    {
+                        Name = "Reetik Chandra",
+                        Email = "reetik@gmail.com",
+                        Phone = "0000000000",
+                        Address = "123 xyz",
+                        Major = "Mathematics",
+                        Degree = "Bachelors",
+                        University = "Miami University",
+                        Interest = "Cyber Security",
+                        Work_Experience = "Tutor",
+                        Gpa = "4",
+                        Graduation_Date = "2023"
+                    },
+
+                    new InfoS
+                    {
+                        Name = "Debjyoti",
+                        Email = "Debjyoti@gmail.com",
+                        Phone = "0000000000",
+                        Address = "123 xyz",
+                        Major = "Finance",
+                        Degree = "Bachelors",
+                        University = "abc University",
+                        Interest = "Consultancy",
+                        Work_Experience = "None",
+                        Gpa = "4",
+                        Graduation_Date = "2022"
+                    },
+
+                    new InfoS
+                    {
+                        Name = "Mounika",
+                        Email = "Mounika@gmail.com",
+                        Phone = "0000000000",
+                        Address = "1234 xyz",
+                        Major = "Computer Science",
+                        Degree = "Masters",
+                        University = "xyz University",
+                        Interest = "Machine Learning",
+                        Work_Experience = "None",
+                        Gpa = "4",
+                        Graduation_Date = "2020"
+                    },
+
+                    new InfoS
+                    {
+                        Name = "Saranya",
+                        Email = "Saranya@gmail.com",
+                        Phone = "0000000000",
+                        Address = "675 xyz",
+                        Major = "Computer Science",
+                        Degree = "Bachelors",
+                        University = "xyz University",
+                        Interest = "Back end",
+                        Work_Experience = "None",
+                        Gpa = "4",
+                        Graduation_Date = "2024"
+                    }
+
+
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
