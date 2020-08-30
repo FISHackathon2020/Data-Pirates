d620cc843749dd9c169cbe4a7ad8e8fc7e8f63fb
Reetik Chandra <61019089+reetik8@users.noreply.github.com>
2020-08-30 08:08:39 -0400

    Add files via upload

---
+++ DataPiratesTeam/Pages/Team.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace DataPiratesTeam.Pages
+{
+    public class TeamModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public TeamModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
